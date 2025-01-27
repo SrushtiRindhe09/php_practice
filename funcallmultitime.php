d369937442dc3b5ec5bc304593b38746665c4b24
Srushti <rindhesrushti2006@gmail.com>
2025-01-27 09:08:33 +0530

    Added 1 function program

---
+++ funcallmultitime.php
@@ -0,0 +1,16 @@
+<?php
+function hello(){
+echo "hello everybody <br>";
+}
+
+function hello1(){
+echo "Welome to php <br>";
+}
+hello();
+hello();
+hello();// call multiple time 
+echo "this is an eample<br>";
+hello();
+hello1();
+
+?>
